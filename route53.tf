ca24b86023d88cdfef5b8d46cd3953b5a40a6258
Colby <colbysmith99@hotmail.com>
2024-07-12 15:13:57 +0100

    www alias
    Added www sub record for the www alias.

--- route53.tf
+++ route53.tf
@@ -25,3 +25,16 @@ resource "aws_route53_record" "root" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "subdomain" {
+  provider = aws.eu_west_1
+
+  zone_id  = data.aws_route53_zone.main.zone_id
+  name     = "www"
+  type     = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
